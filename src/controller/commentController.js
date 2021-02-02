3dfc6b99a8be153f30a14ade53c19f5018098ce5
Ayoub.Gharbi <gharbi.ayoub@gmail.com>
2021-02-02 12:33:45 +0100

    added update and delete comment

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -6,11 +6,13 @@ module.exports = class CommentController {
     this.router = express.Router();
     this.commentRepository = commentRespository;
     this.router.post("/", this.addComment);
+    this.router.delete("/", this.deleteComment);
+    this.router.patch("/", this.updateComment);
     this.router.get("/", this.getPostComments);
   }
   getPostComments = async (req, res) => {
     const { postId } = url.parse(req.url, true).query;
-    console.log(postId);
+    // console.log(postId);
     try {
       if (postId) {
         const comments = await this.commentRepository
@@ -51,6 +53,53 @@ module.exports = class CommentController {
         comment.post
       ) {
         await this.commentRepository.save(comment);
+        res.status(HttpStatus.StatusCodes.CREATED).json({ message: "OK" });
+      } else {
+        res
+          .status(HttpStatus.StatusCodes.BAD_REQUEST)
+          .json({ message: "missing query parameters" });
+      }
+    } catch (error) {
+      // console.log(error);
+      res
+        .status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "error" });
+    }
+  };
+
+  updateComment = async (req, res) => {
+    const comment = req.body;
+    try {
+      if (comment && comment.content && comment.id && comment.date) {
+        await this.getRepository(Comment)
+          .createQueryBuilder()
+          .update(Comment)
+          .set(comment)
+          .where("id = :id", { id: comment.id })
+          .execute();
+        res.status(HttpStatus.StatusCodes.CREATED).json({ message: "OK" });
+      } else {
+        res
+          .status(HttpStatus.StatusCodes.BAD_REQUEST)
+          .json({ message: "missing query parameters" });
+      }
+    } catch (error) {
+      // console.log(error);
+      res
+        .status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "error" });
+    }
+  };
+  deleteComment = async (req, res) => {
+    try {
+      const { id } = url.parse(req.url, true).query;
+      if (id) {
+        await this.postRepository
+          .createQueryBuilder()
+          .delete()
+          .from(Comment)
+          .where("id = :id", { id })
+          .execute();
         res.status(HttpStatus.StatusCodes.CREATED).json({ message: "OK" });
       } else {
         res
